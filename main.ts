3874b83e4b6faf5bead38b5c9c65bf99cc10d0e9
Mr. Roth-Ritchie <jrothritchie@hdsd.org>
2021-02-04 17:40:44 -0500

    Added some basic code

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-WrappedMaqueen.setMotors(-1, 1)
+input.onButtonPressed(Button.A, function () {
+    WrappedMaqueen.setMotors(0, 0)
+})
+input.onButtonPressed(Button.AB, function () {
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+})
+input.onButtonPressed(Button.B, function () {
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 255)
+})
+basic.forever(function () {
+    basic.showNumber(DFRobotMaqueenPlus.readSpeed(Motors1.M1))
+})
